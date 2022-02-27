96b5fb27b517a99a5f04994da0b77d149896189b
Tools <eray.varyeter@outlook.com>
2022-02-27 23:23:21 +0100

    add histogram GPU tests

--- image-processing/histogram/src/histogram.cpp
+++ image-processing/histogram/src/histogram.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/core/base.hpp>
 
 #include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
 #include <opencv2/cudacodec.hpp>
 
 #include "histogram/histogram.h"
@@ -40,10 +41,29 @@ HistogramHandler::NormalizeHistogram(cv::Mat& channel, double alpha, double beta
 	return normalized;
 }
 
+cv::cuda::GpuMat 
+HistogramHandler::NormalizeHistogram(cv::cuda::GpuMat& channel, double alpha, double beta) {
+	ASSERT(channel.channels() == 1, "Input histogram must have 1 channel", base::Logger::Severity::Error);
+	cv::cuda::GpuMat normalized;
+	cv::cuda::normalize(channel, normalized, alpha, beta, cv::NORM_MINMAX, -1, cv::Mat());
+	return normalized;
+}
+
 double 
 HistogramHandler::CompareHistogram(cv::Mat& hist1, cv::Mat& hist2, HistogramComparisonMetrics method) {
 	ASSERT(hist1.channels() == 1 && hist2.channels() == 1, "Input histograms must have 1 channel", base::Logger::Severity::Error);
 	return cv::compareHist(hist1, hist2, method);
+}
+
+double 
+HistogramHandler::CompareHistogram(cv::cuda::GpuMat& hist1, cv::cuda::GpuMat& hist2, HistogramComparisonMetrics method) {
+	ASSERT(hist1.channels() == 1 && hist2.channels() == 1, "Input histograms must have 1 channel", base::Logger::Severity::Error);
+	cv::Mat cpuHist1, cpuHist2;
+	hist1.download(cpuHist1);
+	hist2.download(cpuHist2);
+	cpuHist1.convertTo(cpuHist1, CV_32FC1);
+	cpuHist2.convertTo(cpuHist2, CV_32FC1);
+	return cv::compareHist(cpuHist1, cpuHist2, method);
 }
 
 cv::Mat 
@@ -63,6 +83,27 @@ HistogramHandler::ApplyHistogramEqualization(cv::Mat& image) {
 		cv::equalizeHist(channels[0], channels[0]);
 		cv::merge(channels, ycrcb);
 		cv::cvtColor(ycrcb, dst, cv::COLOR_YCrCb2BGR);
+	}
+	return dst;
+}
+
+cv::cuda::GpuMat 
+HistogramHandler::ApplyHistogramEqualization(cv::cuda::GpuMat& image) {
+	ASSERT((image.channels() == 1 || image.channels() == 3), "Input image must have either 1 or 3 channels", base::Logger::Severity::Error);
+	cv::cuda::GpuMat dst;
+	// gray image
+	if (image.channels() == 1) {
+		cv::cuda::equalizeHist(image, dst);
+	}
+	// color image
+	else {
+		cv::cuda::GpuMat ycrcb;
+		cv::cuda::cvtColor(image, ycrcb, cv::COLOR_BGR2YCrCb);
+		std::vector<cv::cuda::GpuMat> channels;
+		cv::cuda::split(ycrcb, channels);
+		cv::cuda::equalizeHist(channels[0], channels[0]);
+		cv::cuda::merge(channels, ycrcb);
+		cv::cuda::cvtColor(ycrcb, dst, cv::COLOR_YCrCb2BGR);
 	}
 	return dst;
 }
@@ -91,6 +132,34 @@ HistogramHandler::ApplyAdaptiveHistogramEqualization(cv::Mat& image, double clip
 		destination.copyTo(lab_planes[0]);
 		cv::merge(lab_planes, labImage);
 		cv::cvtColor(labImage, dst, cv::COLOR_Lab2BGR);
+	}
+	return dst;
+}
+
+cv::cuda::GpuMat 
+HistogramHandler::ApplyAdaptiveHistogramEqualization(cv::cuda::GpuMat& image, double clipLimit) {
+	ASSERT((image.channels() == 1 || image.channels() == 3), "Input image must have either 1 or 3 channels", base::Logger::Severity::Error);
+	cv::cuda::GpuMat dst;
+	cv::Ptr<cv::cuda::CLAHE> clahe = cv::cuda::createCLAHE();
+	clahe->setClipLimit(clipLimit);
+
+	// gray image
+	if (image.channels() == 1) {
+		clahe->apply(image, dst);
+	}
+	else {
+		cv::cuda::GpuMat labImage;
+		cv::cuda::cvtColor(image, labImage, cv::COLOR_BGR2Lab);
+		// Extract the L channel
+		std::vector<cv::cuda::GpuMat> lab_planes(3);
+		cv::cuda::split(labImage, lab_planes);
+		// apply the CLAHE algorithm to the L channel
+		cv::cuda::GpuMat destination;
+		clahe->apply(lab_planes[0], destination);
+		// Merge the the color planes back into an Lab image
+		destination.copyTo(lab_planes[0]);
+		cv::cuda::merge(lab_planes, labImage);
+		cv::cuda::cvtColor(labImage, dst, cv::COLOR_Lab2BGR);
 	}
 	return dst;
 }
--- image-processing/histogram/test/Fixture.h
+++ image-processing/histogram/test/Fixture.h
@@ -4,6 +4,9 @@
 #include <file-storage/file-storage.h>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/cudacodec.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,14 +18,29 @@ public:
 	Fixture() {
         m_resourceDirectory = base::File::GetExecutablePath() / ".." / ".." / ".." / ".." / ".."
             / "image-processing" / "histogram" / "resource";
+        
         m_colorImage1 = cv::imread(m_resourceDirectory / "Lena.jpg");
         m_colorImage2 = cv::imread(m_resourceDirectory / "SheldonCooper.jpg");
         m_grayImage1 = cv::imread(m_resourceDirectory / "Lena.jpg", cv::IMREAD_GRAYSCALE);
         m_grayImage2 = cv::imread(m_resourceDirectory / "SheldonCooper.jpg", cv::IMREAD_GRAYSCALE);
+
+        m_colorImage1Gpu.upload(m_colorImage1);
+        m_colorImage2Gpu.upload(m_colorImage2);
+        m_grayImage1Gpu.upload(m_grayImage1);
+        m_grayImage2Gpu.upload(m_grayImage2);
+
         ASSERT((m_colorImage1.channels() == 3), "Image 1 must be 3 channel BGR image", base::Logger::Severity::Error);
+        ASSERT((m_colorImage1Gpu.channels() == 3), "Image 1 must be 3 channel BGR image", base::Logger::Severity::Error);
+
         ASSERT((m_colorImage2.channels() == 3), "Image 2 must be 3 channel BGR image", base::Logger::Severity::Error);
+        ASSERT((m_colorImage2Gpu.channels() == 3), "Image 2 must be 3 channel BGR image", base::Logger::Severity::Error);
+
         ASSERT((m_grayImage1.channels() == 1), "Gray Image 1 must be 1 channel grayscale image", base::Logger::Severity::Error);
+        ASSERT((m_grayImage1Gpu.channels() == 1), "Gray Image 1 must be 1 channel grayscale image", base::Logger::Severity::Error);
+
         ASSERT((m_grayImage2.channels() == 1), "Gray Image 2 must be 1 channel grayscale image", base::Logger::Severity::Error);
+        ASSERT((m_grayImage2Gpu.channels() == 1), "Gray Image 2 must be 1 channel grayscale image", base::Logger::Severity::Error);
+
         m_histogramHandler = std::make_shared<imgproc::HistogramHandler>();
 
         auto xml = m_resourceDirectory / "HistogramResults.xml";
@@ -60,11 +78,37 @@ public:
         }
     }
 
+    bool CheckImagesEqual(const cv::cuda::GpuMat Mat1, const cv::cuda::GpuMat Mat2) {
+        if (Mat1.channels() == 1 && Mat2.channels() == 1) {
+            cv::cuda::GpuMat diff;
+            cv::cuda::absdiff(Mat1, Mat2, diff);
+            return cv::cuda::countNonZero(diff) == 0;
+        }
+        else {
+            std::vector<cv::cuda::GpuMat> channels1, channels2;
+            cv::cuda::split(Mat1, channels1);
+            cv::cuda::split(Mat2, channels2);
+            for (size_t i = 0; i < channels1.size(); ++i) {
+                cv::cuda::GpuMat diff;
+                cv::cuda::absdiff(channels1[i], channels2[i], diff);
+                auto equal = cv::cuda::countNonZero(diff) == 0;
+                if (!equal)
+                    return false;
+            }
+            return true;
+        }
+    }
+
     // Member variables
     cv::Mat m_colorImage1;
     cv::Mat m_colorImage2;
     cv::Mat m_grayImage1;
     cv::Mat m_grayImage2;
+
+    cv::cuda::GpuMat m_colorImage1Gpu;
+    cv::cuda::GpuMat m_colorImage2Gpu;
+    cv::cuda::GpuMat m_grayImage1Gpu;
+    cv::cuda::GpuMat m_grayImage2Gpu;
 
     std::string m_resourceDirectory = "";
 
